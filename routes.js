3fe2a68458c2af814c1e128eee3ab64961cacbc1
Timothy Tolley <timothytolley@outlook.com>
2018-01-19 12:14:10 +1300

    added doom and creatures

--- routes.js
+++ routes.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const fs = require('fs')
 const path = require('path')
+const data = require("./data.json")
 
 const router = express.Router()
 
@@ -13,7 +14,18 @@ router.get('/sacrifice', (req, res) => {
 })
 
 router.get('/creature', (req, res) => {
-  res.render('creature')
+  let postDataTwo = "Bird"
+  let postDataOne = "Snake"
+  let postData = [postDataOne, postDataTwo]
+  creaturesData= data.creatures
+  for (let i = 0; i < creaturesData.length; i++) {
+    if(creaturesData[i].pairing[0] === postData[0] && creaturesData[i].pairing[1] === postData[1]){
+      creature = creaturesData[i] 
+      res.render('creature', creature)
+    }
+  }
 })
+
+
 
 module.exports = router
